7b2e67b10684e27fd79b7b7674aaee1310126fae
Blake <blake.lemarr@outlook.com>
2026-04-04 05:22:49 -0400

    made edits to path construction in storage service. created on test case for each service method. all tests passed. More complex ones will wait until later.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,9 +6,8 @@ Description: Fixtures and other configurations for unit and integration tests.
 
 # ===== IMPORTS =====
 import pytest
-from app.models import (
-    AccountInternal,
-    AccountPublic,
-    AccountStatus
-)
+from app.models import *
+from app.services import *
+from app.utilities import *
+from app.controllers import *
 # ===================
