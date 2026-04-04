224a3c035cddb684ba9613f464fd88698099bbc8
Blake <blake.lemarr@outlook.com>
2026-04-04 03:39:17 -0400

    created initial directories and files

---
+++ src/tests/conftest.py
@@ -0,0 +1,14 @@
+'''
+Name: Blake Lemarr
+Updated: 04.04.2026
+Description: Fixtures and other configurations for unit and integration tests.
+'''
+
+# ===== IMPORTS =====
+import pytest
+from app.models import (
+    AccountInternal,
+    AccountPublic,
+    AccountStatus
+)
+# ===================
