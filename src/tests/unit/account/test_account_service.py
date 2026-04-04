bc2cc35b5812eba891e977c635ca8009fbe6960f
Blake <blake.lemarr@outlook.com>
2026-04-04 13:58:10 -0400

    all tests passed for .update()

--- src/tests/unit/account/test_account_service.py
+++ src/tests/unit/account/test_account_service.py
@@ -30,3 +30,30 @@ def test_query_users(account_service, account_factory):
 
     users: list[AccountInternal] = [account_service.query_users('username', user.username) for user in created_users]
     assert len(users) == len(created_users)
+
+def test_update(account_service, account_factory):
+    user: AccountInternal = account_factory(username='nonametester')
+
+    created = account_service.create(user)
+    assert created and created is not None
+
+    changes: AccountInternal = account_factory()
+    changes.id = user.id
+    assert changes.username != user.username
+    assert changes.pii_email != user.pii_email
+    assert changes.hashed_password != user.hashed_password
+    assert changes.id == user.id
+
+    updated = account_service.update('username', 'nonametester', changes)
+    assert updated and updated is not None
+
+    updated_user = account_service.query_user('id', user.id)
+    assert isinstance(updated_user, AccountInternal)
+    assert updated_user == changes
+    assert updated_user != user
+
+    old_user_data = account_service.query_user('username', user.username)
+    assert old_user_data is None
+
+    duplicate_users = account_service.query_users('id', user.id)
+    assert len(duplicate_users) == 1
