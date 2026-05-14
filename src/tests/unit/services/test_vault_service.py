81c5e4b5ffe0eea614cb168d819de8d33706026d
Blake <blake.lemarr@outlook.com>
2026-05-14 09:44:18 -0400

    created test case base

---
+++ src/tests/unit/services/test_vault_service.py
@@ -0,0 +1,13 @@
+'''
+AUTHOR: Blake Lemarr
+DATE: 05.14.26
+DESCRIPTION: Test cases for vault service
+'''
+
+# ------------ IMPORTS ------------ #
+import json
+import pytest
+
+from app.models import VaultModel, VaultEntryModel, VaultLoginDataModel
+
+
