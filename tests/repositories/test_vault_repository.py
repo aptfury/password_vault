040df5bd86ce5d712d8ab2aa98a7a942abf22d40
Blake <blake.lemarr@outlook.com>
2026-05-18 00:02:34 -0400

    refactored to accommodate conftest generate_user changes

--- tests/repositories/test_vault_repository.py
+++ tests/repositories/test_vault_repository.py
@@ -11,64 +11,64 @@ from src.app.repositories.vault_repository import VaultRepo
 # ------------ class ------------ #
 class TestVaultRepository:
     def test_create(self, vault_repo, generate_user):
-        _, vault = generate_user()
+        user = generate_user()
         repo: VaultRepo = vault_repo
         repo.db.initialized()
         
-        success: bool = repo.create(vault)
+        success: bool = repo.create(user['vault'])
         
         assert success
         
     def test_get(self, vault_repo, generate_user):
-        _, vault = generate_user()
+        user = generate_user()
         repo: VaultRepo = vault_repo
         repo.db.initialized()
         
-        success: bool = repo.create(vault)
+        success: bool = repo.create(user['vault'])
         
         assert success
 
-        res: VaultModel = repo.get('_id', vault.id)
+        res: VaultModel = repo.get('_id', user['vault'].id)
         
-        assert vault == res
+        assert user['vault'] == res
         
     def test_get_id(self, vault_repo, generate_user):
-        _, vault = generate_user()
+        user = generate_user()
         repo: VaultRepo = vault_repo
         repo.db.initialized()
         
-        success: bool = repo.create(vault)
+        success: bool = repo.create(user['vault'])
         
         assert success
         
-        res: str = repo.get_id('_id', vault.id)
+        res: str = repo.get_id('_id', user['vault'].id)
         
-        assert res == vault.id
+        assert res == user['vault'].id
         
     def test_update(self, vault_repo, generate_user):
-        _, vault = generate_user()
+        user = generate_user()
         repo: VaultRepo = vault_repo
         repo.db.initialized()
         
-        success: bool = repo.create(vault)
+        success: bool = repo.create(user['vault'])
         
         assert success
         
-        _, vault_two = generate_user()
+        user_two = generate_user()
         
-        success: bool = repo.update('_id', vault.id, vault_two)
+        success: bool = repo.update('_id', user['vault'].id, user_two['vault'])
         
         assert success
         
     def test_delete(self, vault_repo, generate_user):
-        _, vault = generate_user()
+        user = generate_user()
         repo: VaultRepo = vault_repo
         repo.db.initialized()
         
-        success: bool = repo.create(vault)
+        success: bool = repo.create(user['vault'])
         
         assert success
         
-        success: bool = repo.delete('_id', vault.id)
+        success: bool = repo.delete('_id', user['vault'].id)
         
         assert success
