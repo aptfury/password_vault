cb8de7f2d697365c4db33d2764dc99b74128b6d3
Blake <blake.lemarr@outlook.com>
2026-05-17 22:01:14 -0400

    created security, session, and auth fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ DESCRIPTION: Config file for tests
 
 # ------------ imports ------------ #
 import pytest
-from uuid import uuid4
 from faker import Faker
 from pathlib import Path
 from datetime import datetime
@@ -14,6 +13,9 @@ from src.app.configs.database import Database
 from src.app.repositories.vault_repository import VaultRepo
 from src.app.repositories.account_repository import AccountRepo
 from src.app.configs.models import AccountModel, PasswordModel, VaultModel, EntryModel
+from src.app.services.session_service import SessionService
+from src.app.services.auth_service import AuthService
+from src.app.utilities.security_utilities import SecurityUtilities
 
 # ------------ configs ------------ #
 pytest_plugins = [
@@ -25,21 +27,22 @@ pytest_plugins = [
 def tmp_database(tmp_path) -> Database:
     test_path: Path = tmp_path / 'accounts.json'
     database: Database = Database(db_path=test_path)
+    
     return database
 
-# ------------ account_repo ------------ #
+# ------------ account repo ------------ #
 @pytest.fixture
-def account_repo(tmp_database, monkeypatch) -> AccountRepo:
+def account_repo(tmp_database) -> AccountRepo:
     repo: AccountRepo = AccountRepo()
-    
-    monkeypatch.setattr(repo, 'db', tmp_database)
+    repo.db = tmp_database
+
     return repo
 
 @pytest.fixture
-def vault_repo(tmp_database, monkeypatch) -> VaultRepo:
+def vault_repo(tmp_database) -> VaultRepo:
     repo: VaultRepo = VaultRepo()
-    
-    monkeypatch.setattr(repo, 'db', tmp_database)
+    repo.db = tmp_database
+
     return repo
 
 # ------------ vault faker ------------ #
@@ -51,7 +54,9 @@ def gen_vault() -> VaultModel:
             _id=fake.uuid4(),
             vault=[]
         )
+        
         return vault
+    
     return _gen_vault
 
 # ------------ account faker ------------ #
@@ -69,5 +74,31 @@ def gen_account() -> AccountModel:
             ),
             created=datetime.now()
         )
+        
         return account
+    
     return _account_vault
+
+# ------------ security utilities ------------ #
+@pytest.fixture
+def security() -> SecurityUtilities:
+    security: SecurityUtilities = SecurityUtilities()
+    
+    return security
+
+# ----------- session service ------------ #
+@pytest.fixture
+def session() -> SessionService:
+    session: SessionService = SessionService()
+    
+    return session
+
+# ------------ auth service ------------ #
+@pytest.fixture
+def auth(session, account_repo, vault_repo, security) -> AuthService:
+    auth: AuthService = AuthService(session=session)
+    auth.account = account_repo
+    auth.vault = vault_repo
+    auth.utils = security
+
+    return auth
