fc1285dd202f8e5712e46266efc7f4894d77b77d
Blake <blake.lemarr@outlook.com>
2026-05-17 23:59:51 -0400

    refactored to accommodate conftest generate_user changes

--- tests/repositories/test_account_repository.py
+++ tests/repositories/test_account_repository.py
@@ -11,68 +11,68 @@ from src.app.repositories.account_repository import AccountRepo
 # ------------ class ------------ #
 class TestAccountRepository:
     def test_create(self, account_repo, generate_user):
-        account, _ = generate_user()
+        user = generate_user()
         repo: AccountRepo = account_repo
         repo.db.initialized()
         
-        success: bool = repo.create(account)
+        success: bool = repo.create(user['account'])
         
         assert success
 
     def test_get(self, account_repo, generate_user):
-        account, _ = generate_user()
+        user  = generate_user()
         repo: AccountRepo = account_repo
         repo.db.initialized()
         
-        success: bool = repo.create(account)
+        success: bool = repo.create(user['account'])
         
         assert success
         
-        res: AccountModel = repo.get('username', account.username)
+        res: AccountModel = repo.get('username', user['account'].username)
         
-        assert account == res
+        assert user['account'] == res
         
     def test_get_id(self, account_repo, generate_user):
-        account, _ = generate_user()
+        user  = generate_user()
         repo: AccountRepo = account_repo
         repo.db.initialized()
         
-        success: bool = repo.create(account)
+        success: bool = repo.create(user['account'])
         
         assert success
         
-        res: str = repo.get_id('username', account.username)
+        res: str = repo.get_id('username', user['account'].username)
         
-        assert account.id == res
+        assert user['account'].id == res
         
     def test_update(self, account_repo, generate_user):
-        account, _ = generate_user()
+        user  = generate_user()
         repo: AccountRepo = account_repo
         repo.db.initialized()
         
-        success: bool = repo.create(account)
+        success: bool = repo.create(user['account'])
         
         assert success
         
-        account_two, _ = generate_user()
-        account_two.id = account.id
-        account_two.email = account.email
-        account_two.password = account.password
-        account_two.created = account.created
+        user_two = generate_user()
+        user_two['account'].id = user['account'].id
+        user_two['account'].email = user['account'].email
+        user_two['account'].password = user['account'].password
+        user_two['account'].created = user['account'].created
 
-        success: bool = repo.update('_id', account.id, account_two)
+        success: bool = repo.update('_id', user['account'].id, user_two['account'])
         
         assert success
 
     def test_delete(self, account_repo, generate_user):
-        account, _ = generate_user()
+        user = generate_user()
         repo: AccountRepo = account_repo
         repo.db.initialized()
         
-        success: bool = repo.create(account)
+        success: bool = repo.create(user['account'])
         
         assert success
         
-        success: bool = repo.delete('username', account.username)
+        success: bool = repo.delete('username', user['account'].username)
         
         assert success
