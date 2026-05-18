a69e41ab5675748abd265e77e15725ba4cc4729a
Blake <blake.lemarr@outlook.com>
2026-05-18 01:11:01 -0400

    created test_auth_service with all tests passed

--- tests/services/test_auth_service.py
+++ tests/services/test_auth_service.py
@@ -27,20 +27,63 @@ class TestAuthService:
         assert account.username == user['account'].username
         assert account.email == user['account'].email
         
-    # def test_login(self, auth):
-    #     fake: Faker = Faker()
-    #     service: AuthService = auth
+    def test_login(self, auth, generate_user):
+        service: AuthService = auth
+
+        fake: Faker = Faker()
+        raw_password: str = fake.password()
+        user: dict = generate_user()
         
-    #     username: str = fake.user_name()
-    #     raw_password: str = fake.password()
-    #     email: str = fake.email()
+        success: bool = service.create_user(user['account'].username, raw_password, user['account'].email)
         
-    #     success: bool = service.create_user(username, raw_password, email)
+        assert success
         
-    #     assert success
+        logged_in: bool = service.login(user['account'].username, raw_password)
+        account: AccountModel = service.account.get('username', user['account'].username)
         
-    #     success: bool = service.login(username, raw_password)
+        assert logged_in
+        assert account.id == service.session.id
+        assert service.utils.session_is_active()
+        assert service.session.session_id is not None
+        assert account.username == service.session.username
         
-    #     assert service.session.session_key is not None
-    #     assert service.session.id is not None
-    #     assert service.session.username is not None
+    def test_access_granted(self, auth, generate_user):
+        service: AuthService = auth
+
+        fake: Faker = Faker()
+        raw_password: str = fake.password()
+        user: dict = generate_user()
+        
+        success: bool = service.create_user(user['account'].username, raw_password, user['account'].email)
+        
+        assert success
+        
+        access_is_false: bool = service.access_granted()
+        
+        assert not access_is_false
+        
+        service.login(user['account'].username, raw_password)
+        
+        access_is_true: bool = service.access_granted()
+        
+        assert access_is_true
+        
+    def test_logout(self, auth, generate_user):
+        service: AuthService = auth
+
+        fake: Faker = Faker()
+        raw_password: str = fake.password()
+        user: dict = generate_user()
+        
+        success: bool = service.create_user(user['account'].username, raw_password, user['account'].email)
+        
+        assert success
+        
+        service.login(user['account'].username, raw_password)
+        logged_out: bool = service.logout()
+        
+        assert logged_out
+        assert service.session.id is None
+        assert service.session.username is None
+        assert service.session.session_id is None
+        assert not service.utils.session_is_active()
