91c05ac709d88a933a5776b79d9090cb8708cf37
Blake <blake.lemarr@outlook.com>
2026-05-17 21:34:53 -0400

    added session and auth set up to main

--- src/app/main.py
+++ src/app/main.py
@@ -5,10 +5,12 @@ DESCRIPTION: The main script.
 '''
 
 # ------------ imports ------------ #
-
+from .services import AuthService
+from .services import SessionService
 
 # ------------ class ------------ #
 def main():
-    pass
+    session: SessionService = SessionService()
+    auth: AuthService = AuthService(session=session)
 
 main()
